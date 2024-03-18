5e464f59853a5d99643ebc86fbb60111b4d59962
dungmktb123 <135578333+dungmktb123@users.noreply.github.com>
2024-03-18 07:46:11 +0700

    first commit

--- E_shop/urls.py
+++ E_shop/urls.py
@@ -2,8 +2,14 @@
 from django.contrib import admin
 from django.urls import path
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', views.BASE, name='base'),
-    path('', views.HOME, name='home')
-]
+    path('', views.HOME, name='home'),
+    path('product', views.PRODUCT, name='product'),
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
--- E_shop/views.py
+++ E_shop/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, redirect
-
+from store_app.models import Product
 
 def BASE(request):
     return render(request, 'Main/base.html')
 
 
 def HOME(request):
-    return render(request, "Main/index.html")
+    product = Product.objects.filter(status='Publish')
+
+    context = {
+        'product': product,
+    }
+    return render(request, "Main/index.html", context)
+
+
+def PRODUCT(request):
+    product = Product.objects.filter(status='P')
+    return render(request, "Main/product.html")
